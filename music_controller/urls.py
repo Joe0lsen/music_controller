fc302df4691f84ea50e4ddc91e6f468296f71324
Joe0lsen <olsen.jcm@gmail.com>
2026-05-05 13:17:07 -0500

    Set up base model

--- music_controller/urls.py
+++ music_controller/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    # path('v1/', include("api.urls")),
+    path('v1/', views.main, name='home')
 ]
